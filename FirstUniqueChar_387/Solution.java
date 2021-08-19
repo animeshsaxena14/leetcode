bb36dc08f5c454709abc18d68f96471d46db6215
animeshsaxena14 <animeshsaxena14@gmail.com>
2021-08-19 19:33:42 +0530

    solution to 387

---
+++ FirstUniqueChar_387/Solution.java
@@ -0,0 +1,20 @@
+package FirstUniqueChar_387;
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] countArr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            countArr[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++){
+            if (countArr[s.charAt(i) - 'a'] == 1) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String ar[]) {
+        Solution solution = new Solution();
+        System.out.println(solution.firstUniqChar("aabb"));
+    }
+}
