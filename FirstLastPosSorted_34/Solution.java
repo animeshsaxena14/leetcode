91c06b80ef1b0b4868f461f34b55d59cfe6a5004
animeshsaxena14 <animeshsaxena14@gmail.com>
2021-08-20 16:14:14 +0530

    updated 34

--- FirstLastPosSorted_34/Solution.java
+++ FirstLastPosSorted_34/Solution.java
@@ -1,2 +1,54 @@
-package FirstLastPosSorted_34;public class Solution {
+package FirstLastPosSorted_34;
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int [] indices = new int [2];
+        indices[0] = findFirstOccurance(nums, target);
+        indices[1] = findLastOccurance(nums, target);
+        return indices;
+
+    }
+
+    public int findFirstOccurance (int[] nums, int target){
+        int index = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end){
+            int midPoint = start + (end-start)/2;
+            if (target > nums[midPoint]){
+                start = midPoint + 1;
+            }
+            if (target <= nums[midPoint]){
+                end = midPoint - 1;
+            }
+            if (target == nums[midPoint]) index = midPoint;
+
+        }
+
+        return index;
+    }
+
+    public int findLastOccurance (int[] nums, int target){
+        int index = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end){
+            int midPoint = start + (end-start)/2;
+            if (target >= nums[midPoint]){
+                start = midPoint + 1;
+            }
+            if (target < nums[midPoint]){
+                end = midPoint - 1;
+            }
+            if (target == nums[midPoint]) index = midPoint;
+
+        }
+
+
+
+        return index;
+
+    }
 }
--- FirstLastPosSorted_34/SolutionTests.java
+++ FirstLastPosSorted_34/SolutionTests.java
@@ -1,2 +1,19 @@
-package FirstLastPosSorted_34;public class SolutionTests {
+package FirstLastPosSorted_34;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolutionTests {
+
+    @Test
+    public void TestFirstLastPosSortedArray(){
+        Solution solution = new Solution();
+        assertArrayEquals(solution.searchRange(new int[] {5,7,7,8,8,10}, 8), new int []{3, 4});
+        assertArrayEquals(solution.searchRange(new int[] {5,7,7,8,8,10}, 6), new int []{-1, -1});
+        assertArrayEquals(solution.searchRange(new int[] {}, 6), new int []{-1, -1});
+        assertArrayEquals(solution.searchRange(new int[] {8,8,8,8,8,8,8,8}, 8), new int []{0, 7});
+
+    }
+
+
 }
